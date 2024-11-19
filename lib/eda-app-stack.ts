e8a3bc5079d69ef20bd9dbcd7f2c274622fcfe4e
Dylan <dylanchai2003@gmail.com>
2024-11-19 11:11:24 +0000

    Completed Fan out Implementation - added emailing

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -24,7 +24,7 @@ export class EDAAppStack extends cdk.Stack {
 
     // Create the SQS queue for image processing
     const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
-      receiveMessageWaitTime: cdk.Duration.seconds(10), // Updated name and wait time
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
 
     // Create the SNS topic for new image events
@@ -47,7 +47,7 @@ export class EDAAppStack extends cdk.Stack {
     // S3 --> SNS (Publish image events to the SNS topic)
     imagesBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
-      new s3n.SnsDestination(newImageTopic) // Changed to SNS
+      new s3n.SnsDestination(newImageTopic)
     );
 
     // SNS --> SQS (Subscribe the image process queue to the topic)
@@ -64,6 +64,42 @@ export class EDAAppStack extends cdk.Stack {
 
     // Grant the Lambda function permission to read from the bucket
     imagesBucket.grantRead(processImageFn);
+
+    // New SQS queue for the mailer function
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    // Lambda function for sending emails
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    // SNS --> SQS (Subscribe the mailer queue to the topic)
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+    // SQS --> Lambda (Trigger mailer function from the mailer queue)
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    });
+    mailerFn.addEventSource(newImageMailEventSource);
+
+    // Grant the mailer Lambda function permission to send emails using SES
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"], // Replace "*" with specific SES verified identities if needed
+      })
+    );
 
     // Output the bucket name
     new cdk.CfnOutput(this, "bucketName", {
